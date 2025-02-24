37f91ee66bed6f8d439724eb2ed2ef756223c869
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2025-02-24 14:02:17 -0500

    Se agregó la clase Archivo

---
+++ src/main/java/SO/Archivo.java
@@ -0,0 +1,13 @@
+package SO;
+
+public class Archivo {
+    private String nombreArchivo;
+    
+    public Archivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+    
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
+}
